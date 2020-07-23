3119d75270921c6048612b834931633b2591953e
Rayaan Grewal <rayaangrewal@gmail.com>
2020-07-23 23:55:33 +0530

    environ secret key added

--- Main/settings.py
+++ Main/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tay=ghfd!nc^(*tw$_t5h4qk1t)(6mqeq&=wt+_2scgnasov_t'
+#SECRET_KEY = 'tay=ghfd!nc^(*tw$_t5h4qk1t)(6mqeq&=wt+_2scgnasov_t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
