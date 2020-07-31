d0e5ad684830b1ab3f514984190ed447870dab62
Rayaan Grewal <rayaangrewal@gmail.com>
2020-07-31 15:57:56 +0530

    Veg Non-Veg fields added

--- Menu/models.py
+++ Menu/models.py
@@ -5,6 +5,8 @@ class Item(models.Model):
     itemName = models.CharField(max_length = 30)
     ingredients = models.CharField(max_length= 80)
     price = models.IntegerField()
+    veg = models.BooleanField(default=True)
+    nonVeg = models.BooleanField(default=False)
 
     def __str__(self):
         return self.itemName
