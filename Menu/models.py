890b92b7a38d8f763e2378581c3b0b1cded1e120
Rayaan Grewal <rayaangrewal@gmail.com>
2020-07-31 16:01:29 +0530

    bug fixes

--- Menu/models.py
+++ Menu/models.py
@@ -5,8 +5,8 @@ class Item(models.Model):
     itemName = models.CharField(max_length = 30)
     ingredients = models.CharField(max_length= 80)
     price = models.IntegerField()
-    veg = models.BooleanField(default=True)
-    nonVeg = models.BooleanField(default=False)
+    veg = models.BooleanField()
+    nonVeg = models.BooleanField()
 
     def __str__(self):
         return self.itemName
